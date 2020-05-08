adab081d3c238f92c14e28ee03eb96a48a77e540
Hugo Magalhães <hello@hugomagalhaes.design>
2020-05-08 02:14:18 +0200

    Utterly lost

--- FramerX-dribbble-shots.framerfx/code/SimpleOverride.tsx
+++ FramerX-dribbble-shots.framerfx/code/SimpleOverride.tsx
@@ -1,11 +1,15 @@
-import { useRef } from "react"
+import { useRef, useCallback, useEffect } from "react"
 import { Data, Override } from "framer";
 
 
 export function CardRef(props): Override {
-    const ref: any = useRef()
-
-    console.log(ref)
+   
+    const ref = useCallback(elem => {
+        if (elem) {
+            console.log(elem.props.children[0].props.height)
+        }
+    }, [])
+    
 
     return {
         ref: ref,
