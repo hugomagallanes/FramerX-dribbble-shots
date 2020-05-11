051fceaa683274f3a7636702eb3167be9e8c217b
Hugo Magalhães <hello@hugomagalhaes.design>
2020-05-11 17:29:43 +0200

    Added animated navtitle icon

--- FramerX-dribbble-shots.framerfx/code/IconNavtitle.tsx
+++ FramerX-dribbble-shots.framerfx/code/IconNavtitle.tsx
@@ -5,16 +5,79 @@ import {
 } from "framer";
 
 
+export const IconNavtitle = ({isActive}) => {
 
-export const IconNavtitle = (props) => {
+
+	const [active,setActive] = React.useState(isActive) 
+
+	// Set initial state based on prop
+	React.useEffect(()=>{
+		setActive(isActive)
+	},[])
+
+	const handleTap = () => {
+		setActive(!active)
+		// console.log(`Changed state to ${active}`)
+	}
+
     return (
 		<Frame
 			size={16}
-			background="transparent"
+			background={null}
+			style={{
+				zIndex: 10
+			}}
+			onTap={handleTap}
+			transition={{
+				duration: .3,
+				ease: "anticipate"
+			}}
 		>
-			<Frame background="#0d0d0d" height={2} width={16} top={2} radius={3}></Frame>
-			<Frame background="#0d0d0d" height={2} width={10} center="y" radius={3}></Frame>
-			<Frame background="#0d0d0d" height={2} width={13} bottom={2} radius={3}></Frame>
+
+			<Frame 
+				radius={3}
+				width={16}
+				height={2}
+				animate={{
+					background: active ? "white" : "0d0d0d",
+					top: active ? 2 : 2,
+					rotate: active ? 45 : 0,
+					width: active ? 14 : 16
+				}}
+				originX={0}
+			></Frame>
+
+
+			<Frame 
+				radius={3}
+				width={10}
+				height={2}
+				background="0d0d0d"
+				animate={{
+					opacity: active ? 0 : 1,
+				}}
+				originX={0}
+				center="y"
+			></Frame>
+
+
+
+			<Frame 
+				radius={3}
+				// width={13}
+				height={2}
+				animate={{
+					background: active ? "white" : "0d0d0d",
+					bottom: active ? 2 : 2,
+					rotate: active ? -45 : 0,
+					width: active ? 14 : 10
+				}}
+				originX={0}
+			></Frame>
 		</Frame>
 	)
+}
+
+IconNavtitle.defaultProps = {
+	isActive: false
 }
